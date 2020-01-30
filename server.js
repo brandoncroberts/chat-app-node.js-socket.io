4aab0b3deea8beea726f035156ba7ccae37a2cc9
brandon roberts <8brandon@protonmail.com>
2020-01-30 21:00:53 +0100

    Add functionality to allow for chat on local host

---
+++ server.js
@@ -0,0 +1,23 @@
+const io = require("socket.io")(3000);
+
+const users = {};
+
+io.on("connection", socket => {
+  console.log("new user", new Date());
+
+  socket.on("new-user", user => {
+    users[socket.id] = user;
+    socket.broadcast.emit("user-connected", user);
+  });
+
+  socket.on("send-chat-message", message => {
+    socket.broadcast.emit("chat-message", {
+      name: users[socket.id],
+      message: message
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
